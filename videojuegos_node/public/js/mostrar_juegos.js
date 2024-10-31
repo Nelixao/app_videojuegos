b2051e92b6d3d6c4626dfe5c8fbb5f1b07914a7c
Erick-Baco <jesusbaco542@gmail.com>
2024-10-30 19:25:20 -0600

    fix(node): carrito de compras corregido

--- videojuegos_node/public/js/mostrar_juegos.js
+++ videojuegos_node/public/js/mostrar_juegos.js
@@ -1,3 +1,9 @@
+// Escuchar los eventos de clic en los botones de consola
+document.querySelector('#xbox').addEventListener('click', () => cargarJuegosPorConsola('Xbox'));
+document.querySelector('#playstation').addEventListener('click', () => cargarJuegosPorConsola('PlayStation'));
+document.querySelector('#nintendo').addEventListener('click', () => cargarJuegosPorConsola('Nintendo'));
+
+
 // Función para hacer la solicitud AJAX y obtener los videojuegos de una consola
 function borrar(){
     const contenedor = document.querySelector('.recomendacion-header');
@@ -7,20 +13,19 @@ function construir(data, contenedor){
 
     if (data.length > 0) {
         data.forEach(juego => {
-
             precio = juego.videojuego_plataformas[0].precio
 
             contenedor.innerHTML += `
                 <div class="card">
                     <div class="card-img">
-                        <img src="${juego.imagen}" alt="${juego.titulo}">
+                        <img src="${juego.imagen}" alt="${juego.titulo} id="portada">
                     </div>
                     <div class="card-info">
-                        <h4 class="card-title">${juego.titulo}</h4>
+                        <h4 class="card-title" id="titulo">${juego.titulo}</h4>
                         <img src="image/icons/estrellas.png" alt="estrellas" class="estrellas">
-                        <p class="precio">$ ${precio}</p>
-                        <a href="#" class="button agregar-carrito" data-id="${juego.data_id}">Añadir al carrito</a>
-                        <a href="#" class="button mostrar-video" data-link="${juego.video_link}">Review</a>
+                        <p class="precio" id="precio">$ ${precio}</p>
+                        <a href="#" class="button agregar-carrito" data-id="${juego.id_videojuego}">Añadir al carrito</a>
+                        <a href="#" class="button mostrar-video" data-link="${juego.trailer}">Review</a>
                     </div>
                 </div>
             `;
@@ -37,29 +42,14 @@ function cargarJuegosPorConsola(consola) {
         .then(response => response.json())
         .then(data => {
             borrar()
-            const contenedor = document.querySelector('.section-opciones');
+            const contenedor = document.querySelector('.opciones');
             contenedor.innerHTML = '';  // Limpiar el contenedor
             construir(data, contenedor)
         })
         .catch(error => console.error('Error:', error));
 }
 
-function cargarJuegosPorID(id) {
-    fetch(`/predeterminado/${id}`)  // Llama a la ruta del backend
-        .then(response => response.json())
-        .then(data => {
-            const contenedor = document.querySelector('.section-opciones');
-            //const primerosTres = data.slice(0, 3);  // Selecciona solo los primeros 3 elementos
-            construir(data, contenedor);
-        })
-        .catch(error => console.error('Error:', error));
-}
 
-
-// Escuchar los eventos de clic en los botones de consola
-document.querySelector('#xbox').addEventListener('click', () => cargarJuegosPorConsola('Xbox'));
-document.querySelector('#playstation').addEventListener('click', () => cargarJuegosPorConsola('PlayStation'));
-document.querySelector('#nintendo').addEventListener('click', () => cargarJuegosPorConsola('Nintendo'));
 
 
 
